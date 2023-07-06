bd2c5f549eeae67c74661158041a751d27c2405e
Kris Ivan <krisztian.ivan@gmail.com>
2023-07-06 17:01:22 +0100

    added placeholder Card structure

--- github-cards-app/src/App.js
+++ github-cards-app/src/App.js
@@ -5,7 +5,11 @@ class Card extends React.Component {
   render() {
     return (
       <div className="github-profile">
-        One GitHub Profile...
+        <img src="https://placehold.it/75" />
+        <div className="info">
+          <div className="name">Name here...</div>
+          <div className="company">Company here...</div>
+        </div>
       </div>
     );
   }
