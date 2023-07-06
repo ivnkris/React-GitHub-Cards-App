9c6cfa3174ed04546641b1796f9dde7eff0f12d8
Kris Ivan <krisztian.ivan@gmail.com>
2023-07-06 16:57:02 +0100

    added boilerplate Card component

--- github-cards-app/src/App.js
+++ github-cards-app/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
 
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="github-profile">
+        One GitHub Profile...
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <div className="header">{this.props.title}</div>
+    return (
+      <div>
+        <div className="header">{this.props.title}</div>
+        <Card />
+      </div>
+    );
   }
 }
 
